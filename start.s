9de16f321821b6b16a218d7f32ef8e7527939f10
C-Elegans <mtnolan2640@gmail.com>
2015-09-02 15:29:06 -0400

    peek methods made inline

--- start.s
+++ start.s
@@ -41,25 +41,25 @@ dummy:
 ;@	str r1,[r0]
 ;@	bx lr
 .globl PUT16
-PUT16:
-	strh r1,[r0]
-	bx lr
-.globl PUT8
-PUT8:
-	strb r1,[r0]
-	bx lr
-.globl GET32
-GET32:
-    ldr r0,[r0]
-    bx lr
-.globl GET8
-GET8:
-	ldrb r0,[r0]
-	bx lr
-.globl GETPC
-GETPC:
-	mov r0,lr
-	bx lr
+;@PUT16:
+;@	strh r1,[r0]
+;@	bx lr
+;@.globl PUT8
+;@PUT8:
+;@	strb r1,[r0]
+;@	bx lr
+;@.globl GET32
+;@GET32:
+;@    ldr r0,[r0]
+;@    bx lr
+;@.globl GET8
+;@GET8:
+;@	ldrb r0,[r0]
+;@	bx lr
+;@.globl GETPC
+;@GETPC:
+;@	mov r0,lr
+;@	bx lr
 .globl enable_irq
 enable_irq:
     mrs r0,cpsr
