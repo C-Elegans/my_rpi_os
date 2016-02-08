495b93daaf5bf3c399b8fec5e124af1b9c5c29a8
C-Elegans <mtnolan2640@gmail.com>
2016-02-07 19:31:42 -0500

    clean up commented code

--- graphics.c
+++ graphics.c
@@ -47,11 +47,6 @@ int same_side(vec2 p, vec2 a, vec2 b, vec2 c){
 	if(cp1*cp2>=0)return 1;
 	return 0;
 }
-/*function SameSide(p1,p2, a,b)
- cp1 = CrossProduct(b-a, p1-a)
- cp2 = CrossProduct(b-a, p2-a)
- if DotProduct(cp1, cp2) >= 0 then return true
- else return false*/
 int inside(ivec2 p, ivec2 a, ivec2 b, ivec2 c){
 	vec2 vp=(vec2){(float)p.x,(float)p.y};
 	vec2 va=(vec2){(float)a.x,(float)a.y};
@@ -59,36 +54,7 @@ int inside(ivec2 p, ivec2 a, ivec2 b, ivec2 c){
 	vec2 vc=(vec2){(float)c.x,(float)c.y};
 	return same_side(vp,va,vb,vc) && same_side(vp,vb,vc,va) && same_side(vp,vc,va,vb);
 }
-/*void triangle(short* framebuffer,vec2 a, vec2 b, vec2 c, short color){
-	
-	float dx = 1.0f/(float)SCREEN_WIDTH;
-	float dy = .99f/(float)SCREEN_HEIGHT;
-	vec2 p, max;
-	
-	p.x = fmin(a.x,b.x);
-	p.x = fmin(p.x,c.x);
-	p.y = fmin(a.y,b.y);
-	p.y = fmin(p.y,c.y);
-	
-	max.x = fmax(a.x,b.x);
-	max.x = fmax(p.x,c.x);
-	max.y = fmax(a.y,b.y);
-	max.y = fmax(p.y,c.y);
-	float x_initial = p.x;
-	for(;p.y<max.y;p.y=p.y+ dy){
-		//puts("y: ");
-		//uart_putint((int)(p.y*SCREEN_HEIGHT));
-		for(p.x=x_initial;p.x<max.x;p.x=p.x+ dx){
-			//puts("x: ");
-			//uart_putint((int)(p.x*SCREEN_WIDTH));
-			
-			if(inside(p,a,b,c)){
-				set_pixelv(framebuffer,(vec2){p.x,p.y},color);
-			}
-		}
-		
-	}
-}*/
+
 void triangle(short* framebuffer,ivec2 a, ivec2 b, ivec2 c, short color){
 	ivec2 p, max;
 	
@@ -103,11 +69,9 @@ void triangle(short* framebuffer,ivec2 a, ivec2 b, ivec2 c, short color){
 	max.y = fmax(p.y,c.y);
 	int x_initial = p.x;
 	for(;p.y<max.y;p.y++){
-		//puts("y: ");
-		//uart_putint((int)(p.y*SCREEN_HEIGHT));
+		
 		for(p.x=x_initial;p.x<max.x;p.x++){
-			//puts("x: ");
-			//uart_putint((int)(p.x*SCREEN_WIDTH));
+			
 			
 			if(inside(p,a,b,c)){
 				set_pixel(framebuffer,p.x,p.y,color);
@@ -116,18 +80,3 @@ void triangle(short* framebuffer,ivec2 a, ivec2 b, ivec2 c, short color){
 		
 	}
 }
-
-/*void line(int x0, int y0, int x1, int y1) {
- 
- int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
- int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1;
- int err = (dx>dy ? dx : -dy)/2, e2;
- 
- for(;;){
- setPixel(x0,y0);
- if (x0==x1 && y0==y1) break;
- e2 = err;
- if (e2 >-dx) { err -= dy; x0 += sx; }
- if (e2 < dy) { err += dx; y0 += sy; }
- }
- }*/
